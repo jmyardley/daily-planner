bee1bfe5e06d79c37296b19d3b98f1744f097b56
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-10 09:54:46 -0500

    added blank row divs

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
-var rightNow = moment();
+var plannerBox = $(".container");
+var rightNow = moment().format("MMMM Do YYYY");
 console.log(rightNow); 
 
 var dateDiv = $("<div>");
 dateDiv.text(rightNow);
 $("#currentDay").append(dateDiv);
+
+var currentTime = moment().format("h");
+console.log(currentTime);
+
+for (var i = 0; i < 9; i++){
+    var hourDiv = $("<div>");
+    hourDiv.attr("class", "row");
+    plannerBox.append(hourDiv);
+}
