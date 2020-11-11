09523ebe81ea789e3b6d3df8836aafca9ffb3c9f
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-11 16:47:58 -0500

    finished local storage functionality

--- Develop/script.js
+++ Develop/script.js
@@ -12,9 +12,7 @@ console.log(currentTime);
 for (var i = 0; i < 9; i++){
     var hourDiv = $("<div>");
     hourDiv.attr("class", "row");
-    hourDiv.attr("id", i.toString());
     plannerBox.append(hourDiv);
-    hourColor(i, hourDiv);
     
     var labels = ["9a", "10a", "11a", "12p", "1p", "2p", "3p", "4p", "5p"];
     var labelDiv = $("<div>");
@@ -22,17 +20,20 @@ for (var i = 0; i < 9; i++){
     labelDiv.text((labels[i]).toString());
     hourDiv.append(labelDiv);
     
-    var eventForm = $("<form>");
-    eventForm.attr("class", "col-md-10");
-    hourDiv.append(eventForm);
-    var eventInput = $("<input>");
-    eventInput.attr("class", "col-md-12");
-    eventInput.attr("type", "text");
-    eventInput.attr("placeholder", "Add event");
-    eventForm.append(eventInput);
+    var hourText = $("<textarea>");
+    hourText.attr("class", "col-sm-10 form-control");
+    var uniqueTextId = "form" + i.toString();
+    hourText.attr("id", uniqueTextId);
+    hourText.attr("rows", "2");
+    hourColor(i, hourText);
+    hourDiv.append(hourText);
+
+
+
 
     var saveBtn = $("<button>");
     saveBtn.attr("class", "saveBtn col-md-1");
+    saveBtn.attr("id", i.toString());
     hourDiv.append(saveBtn); 
 }
 
@@ -42,8 +43,55 @@ function hourColor(hour, div){
     } else if (hour + 9 > currentTime){
         div.css("background-color", "teal");
     } else {
-        div.css("background-color", "DodgerBlue");
+        div.css("background-color", "LightCoral");
     }
     console.log(currentTime);
     console.log(hour);
 }
+
+$("#0").on("click", function(){
+    var input0 = $("#form0").val();
+    localStorage.setItem("input0", input0);
+});
+$("#1").on("click", function(){
+    var input1 = $("#form1").val();
+    localStorage.setItem("input1", input1);
+});
+$("#2").on("click", function(){
+    var input2 = $("#form2").val();
+    localStorage.setItem("input2", input2);
+});
+$("#3").on("click", function(){
+    var input3 = $("#form3").val();
+    localStorage.setItem("input3", input3);
+});
+$("#4").on("click", function(){
+    var input4 = $("#form4").val();
+    localStorage.setItem("input4", input4);
+});
+$("#5").on("click", function(){
+    var input5 = $("#form5").val();
+    localStorage.setItem("input5", input5);
+});
+$("#6").on("click", function(){
+    var input6 = $("#form6").val();
+    localStorage.setItem("input6", input6);
+});
+$("#7").on("click", function(){
+    var input7 = $("#form7").val();
+    localStorage.setItem("input7", input7);
+});
+$("#8").on("click", function(){
+    var input8 = $("#form8").val();
+    localStorage.setItem("input8", input8);
+});
+
+$("#form0").val(localStorage.getItem("input0"));
+$("#form1").val(localStorage.getItem("input1"));
+$("#form2").val(localStorage.getItem("input2"));
+$("#form3").val(localStorage.getItem("input3"));
+$("#form4").val(localStorage.getItem("input4"));
+$("#form5").val(localStorage.getItem("input5"));
+$("#form6").val(localStorage.getItem("input6"));
+$("#form7").val(localStorage.getItem("input7"));
+$("#form8").val(localStorage.getItem("input8"));
