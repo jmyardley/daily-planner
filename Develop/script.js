0f9c590acd70ada9778ce4b5aa0f3ea13996b843
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-10 21:56:38 -0500

    added logic to format hourly divs based on current time

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,22 @@ var dateDiv = $("<div>");
 dateDiv.text(rightNow);
 $("#currentDay").append(dateDiv);
 
-var currentTime = moment().format("h");
+var currentTime = moment().format("H");
 console.log(currentTime);
 
 for (var i = 0; i < 9; i++){
     var hourDiv = $("<div>");
     hourDiv.attr("class", "row");
     plannerBox.append(hourDiv);
+    hourColor(i, hourDiv);
+}
+
+function hourColor(hour, div){
+    if (hour + 9 === rightNow){
+        div.css("background-color", "DodgerBlue");
+    } else if ( hour + 9 > rightNow){
+        div.css("background-color", "teal");
+    } else {
+        div.css("background-color", "Silver");
+    }
 }
